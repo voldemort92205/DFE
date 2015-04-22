1435bbf501c5add72950d2d9deffbf838b12514d
voldemort92205 <voldemort92205@gmail.com>
2015-04-22 15:56:56 +0800

    image read write, and no use jpeg

--- project2/stitching/src/main.rs
+++ project2/stitching/src/main.rs
@@ -1,6 +1,28 @@
-
+#[warn(unused_imports)]
 extern crate image;
 
+
+use std::fs::File;
+use std::path::Path;
+use std::env;
+//use image::GenericImage;
+
+
+use image :: {
+	GenericImage,
+	ImageBuffer,
+	imageops
+};
+
 fn main() {
-    println!("Hello, world!");
+
+	let args: Vec<_> = env::args().collect();
+	if args.len() > 1 {
+		let img = image::open(&Path::new(&args[1])).unwrap();
+		println!("dimensions{:?}", img.dimensions());
+		
+		println!("{:?}", img.color());
+		let ref mut fout = File::create(&Path::new("out.png")).unwrap();
+		let _ = img.save(fout, image::PNG);
+	}
 }
